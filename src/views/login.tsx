87fa1b148fb8313caeaabb0705364855b527095b
Rafael Zimmer <rzimmerdev@gmail.com>
2023-06-09 00:37:59 -0300

    Forgot login page

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import './login.css';
+
 function Login() {
     return (
-        <div></div>
+        <div id="login">
+            <div className="login-component" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/login-background.jpg'})` }}>
+                <form>
+                    <h2>Login</h2>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" required/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" required/>
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
     );
 }
 
