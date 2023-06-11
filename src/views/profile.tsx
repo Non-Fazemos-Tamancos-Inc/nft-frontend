66fde42187b89c85864e7d8b2eb92d61698f292e
AdaltonF <adaltonfilho2003@gmail.com>
2023-06-11 17:10:45 -0300

    profile incomplete

--- src/views/profile.tsx
+++ src/views/profile.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import './profile.css';
+
 function Profile() {
     return (
-        <div></div>
+        <div id="profile">
+            <h1>Profile</h1>
+            <img className="profile-pic" src="/img/profile_pic.png" alt="Profile picture" />
+        </div>
+        
     );
 }
 
